4f6b94cb54a2f0a00e92a96bea96296f5d82a1ba
hzhu <hz@henryzhu.me>
2025-01-27 22:38:21 -0800

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import {
+  useWallets,
+  useConnectWallet,
+  useSolanaWallets,
+} from "@privy-io/react-auth";
+
+export default function Home() {
+  const { connectWallet } = useConnectWallet();
+  const { wallets: ethereumWallets } = useWallets();
+  const { wallets: solanaWallets } = useSolanaWallets();
+
+  return (
+    <div className="p-4">
+      <button onClick={connectWallet}>connect</button>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <ul>
+          {solanaWallets.map((wallet, index) => {
+            return (
+              <li
+                key={wallet.meta.id}
+                className={index === 0 ? "font-bold text-green-600" : ""}
+              >
+                {wallet.meta.name} - {wallet.address}
+              </li>
+            );
+          })}
+          {ethereumWallets.map((wallet, index) => {
+            return (
+              <li
+                key={wallet.meta.id}
+                className={index === 0 ? "font-bold text-green-600" : ""}
+              >
+                {wallet.meta.name} - {wallet.address}
+              </li>
+            );
+          })}
+        </ul>
+      </main>
+    </div>
+  );
+}
---
+++ src/app/provider.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { base, mainnet } from "wagmi/chains";
+import { PrivyProvider } from "@privy-io/react-auth";
+import { http, createConfig, WagmiProvider } from "wagmi";
+import { toSolanaWalletConnectors } from "@privy-io/react-auth/solana";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  metaMask,
+  injected,
+  walletConnect,
+  coinbaseWallet,
+} from "wagmi/connectors";
+
+const solanaConnectors = toSolanaWalletConnectors({
+  // By default, shouldAutoConnect is enabled
+  shouldAutoConnect: false,
+});
+
+const queryClient = new QueryClient();
+
+const projectId = "ecf05e6e910a7006159c69f03dafbaeb";
+
+export const config = createConfig({
+  chains: [mainnet, base],
+  connectors: [
+    injected(),
+    metaMask(),
+    coinbaseWallet(),
+    walletConnect({ projectId }),
+  ],
+  transports: {
+    [mainnet.id]: http(
+      "https://eth-mainnet.g.alchemy.com/v2/YViRFlzFSftOMSgTV6oTNTOcDH3EnD2a" // demo key
+    ),
+    [base.id]: http(
+      "https://base-mainnet.g.alchemy.com/v2/YViRFlzFSftOMSgTV6oTNTOcDH3EnD2a" // demo key
+    ),
+  },
+});
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <PrivyProvider
+          appId="cm1k78nl101dtqj2bcs76kzab" // test app id
+          config={{
+            appearance: {
+              theme: "light",
+              accentColor: "#676FFF",
+              logo: "https://your-logo-url",
+              walletChainType: "ethereum-and-solana",
+            },
+            embeddedWallets: {
+              createOnLogin: "users-without-wallets",
+            },
+            externalWallets: { solana: { connectors: solanaConnectors } },
+          }}
+        >
+          {children}
+        </PrivyProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
